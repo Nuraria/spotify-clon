6f7a180d634826381849855166c2bed7ae2a404b
Nuraria <nurarihuonnomago1@email.com>
2025-02-02 17:12:52 +0200

    add playlist amd album

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
-        <Navbar />
-    </>
-  )
-}
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
 
-export default DisplayHome
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Тренды</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((song, index) => (
+            <SongItem
+              key={index}
+              image={song.image}
+              name={song.name}
+              desc={song.desc}
+              id={song.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
