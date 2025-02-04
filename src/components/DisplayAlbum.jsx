287e12b97b3cbc779d2ea796cdb348c216885418
Nuraria <nurarihuonnomago1@email.com>
2025-02-04 12:59:34 +0200

    display duration with Id and the previous button and the next button

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { albumsData, assets, songsData } from "../assets/assets";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumsData[id];
+  const { playWithId } = useContext(PlayerContext);
 
   return (
     <>
@@ -41,6 +44,7 @@ const DisplayAlbum = () => {
       <hr />
       {songsData.map((item, index) => (
         <div
+          onClick={() => playWithId(item.id)}
           key={index}
           className="grid grid-cols-3 sm:grid-cols-4 gap-4 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
         >
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,8 +3,18 @@ import { useContext } from "react";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { seekBar, seekBg, playStatus, play, pause, track, time } =
-    useContext(PlayerContext);
+  const {
+    seekBar,
+    seekBg,
+    playStatus,
+    play,
+    pause,
+    track,
+    time,
+    previous,
+    next,
+    seekSong,
+  } = useContext(PlayerContext);
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
@@ -23,6 +33,7 @@ const Player = () => {
             alt="shuffle"
           />
           <img
+            onClick={previous}
             className="w-4 cursor-pointer"
             src={assets.prev_icon}
             alt="prev"
@@ -44,6 +55,7 @@ const Player = () => {
           )}
 
           <img
+            onClick={next}
             className="w-4 cursor-pointer"
             src={assets.next_icon}
             alt="next"
@@ -55,9 +67,12 @@ const Player = () => {
           />
         </div>
         <div className="flex items-center gap-5">
-          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+          <p>
+            {time.currentTime.minute}:{time.currentTime.second}
+          </p>
           <div
             ref={seekBg}
+            onClick={seekSong}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
           >
             <hr
@@ -65,7 +80,9 @@ const Player = () => {
               className="h-1 border-none w-0 bg-green-800 rounded-full"
             />
           </div>
-          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
+          <p>
+            {time.totalTime.minute}:{time.totalTime.second}
+          </p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
